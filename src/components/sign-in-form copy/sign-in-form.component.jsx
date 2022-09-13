672af0d929bdb4da26778dc57488757440f260dc
Dave <dlp140@gmail.com>
2022-09-13 14:26:33 -0400

    user context

--- src/components/sign-in-form copy/sign-in-form.component.jsx
+++ src/components/sign-in-form copy/sign-in-form.component.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 
 import FormInput from '../form-input/form-input.component'
 import Button from '../button/button.component'
+
+import { UserContext } from '../../contexts/user.context'
 
 import './sign-in-form.styles.scss'
 
@@ -20,7 +22,7 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(deafultFormFields)
   const { email, password } = formFields
 
-  console.log(formFields)
+  const { setCurrentUser } = useContext(UserContext)
 
   const resetFormFields = () => {
     setFormFields(deafultFormFields)
@@ -35,8 +37,10 @@ const SignInForm = () => {
     event.preventDefault()
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(email, password)
-      console.log(response)
+      const { user } = await signInAuthUserWithEmailAndPassword(email, password)
+
+      setCurrentUser(user)
+
       resetFormFields()
     } catch (error) {
       switch (error.code) {
